a98f073553d684c4741f5efb68eac13b96790487
wscouto2502 <wevertondasilvacouto@gmail.com>
2025-02-26 07:53:04 -0300

    colocado 20 segundos

--- mouse.py
+++ mouse.py
@@ -3,6 +3,6 @@ import pyautogui
 
 while True:
     pyautogui.move(10, 0)  # Move 10 pixels para a direita
-    time.sleep(240)
+    time.sleep(20)
     pyautogui.move(-10, 0)  # Move 10 pixels para a esquerda
-    time.sleep(240)
+    time.sleep(20)
