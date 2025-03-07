1087371b2a3e1e7e42b34b251dc799c98274e2b5
wscouto2502 <wevertondasilvacouto@gmail.com>
2025-03-07 16:11:31 -0300

    upgrade print

--- mouse.py
+++ mouse.py
@@ -3,6 +3,8 @@ import pyautogui
 
 while True:
     pyautogui.move(10, 0)  # Move 10 pixels para a direita
+    print('movido para direita')
     time.sleep(20)
     pyautogui.move(-10, 0)  # Move 10 pixels para a esquerda
+    print('movido para esquerda')
     time.sleep(20)
